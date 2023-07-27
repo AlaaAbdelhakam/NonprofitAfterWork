ae3b7369f589ff94107d38b885be16130784a782
a-mohamed <a.mohamed@wakeb.tech>
2023-07-27 11:06:45 +0300

    finished

--- app/Http/Controllers/CalenderController.php
+++ app/Http/Controllers/CalenderController.php
@@ -4,62 +4,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\CalendarEvent;
+use App\Models\CrudEvents;
 
-class CalendarController extends Controller
+class CalenderController extends Controller
 {
     public function index()
     {
-        return view('calendar');
+        $events = CrudEvents::all();
+        return view('calender', compact('events'));
     }
-
     public function getEvents()
     {
-        $events = CalendarEvent::all();
+        $events = CrudEvents::all();
 
         $formattedEvents = [];
 
         foreach ($events as $event) {
             $formattedEvent = [
                 'id' => $event->id,
-                'title' => $event->title,
-                'start' => $event->start,
-                'end' => $event->end,
+                'title' => $event->event_name,
+                'start' => $event->event_start,
+                'end' => $event->event_end,
             ];
 
             $formattedEvents[] = $formattedEvent;
         }
 
+        // dd($formattedEvents); // Add this line to inspect the data
+
         return response()->json($formattedEvents);
     }
 
-    public function storeEvent(Request $request)
+    public function store(Request $request)
     {
-        $event = CalendarEvent::create([
-            'title' => $request->title,
-            'start' => $request->start,
-            'end' => $request->end,
+        $event = CrudEvents::create([
+            'event_name' => $request->event_name,
+            'event_start' => $request->event_start,
+            'event_end' => $request->event_end,
         ]);
 
         return response()->json($event);
     }
 
-    public function updateEvent(Request $request, $id)
+    public function update(Request $request, $id)
     {
-        $event = CalendarEvent::findOrFail($id);
+        $event = CrudEvents::findOrFail($id);
 
         $event->update([
-            'title' => $request->title,
-            'start' => $request->start,
-            'end' => $request->end,
+            'event_name' => $request->event_name,
+            'event_start' => $request->event_start,
+            'event_end' => $request->event_end,
         ]);
 
         return response()->json($event);
     }
 
-    public function deleteEvent($id)
+    public function destroy($id)
     {
-        $event = CalendarEvent::findOrFail($id);
+        $event = CrudEvents::findOrFail($id);
         $event->delete();
 
         return response()->json($event);
--- resources/views/calender.blade.php
+++ resources/views/calender.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fullcalendar</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" />
+    <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
+
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" /> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
 </head>
@@ -24,84 +26,91 @@
     <script>
         $(document).ready(function () {
             var SITEURL = "{{ url('/') }}";
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-            var calendar = $('#full_calendar_events').fullCalendar({
-                editable: true,
-                events: SITEURL + "/calendar-events",
-                displayEventTime: true,
-                eventRender: function (event, element, view) {
-                    if (event.allDay === 'true') {
-                        event.allDay = true;
-                    } else {
-                        event.allDay = false;
-                    }
-                },
-                selectable: true,
-                selectHelper: true,
-                select: function (event_start, event_end, allDay) {
-                    var event_name = prompt('Event Name:');
-                    if (event_name) {
-                        var event_start = $.fullCalendar.formatDate(event_start, "Y-MM-DD HH:mm:ss");
-                        var event_end = $.fullCalendar.formatDate(event_end, "Y-MM-DD HH:mm:ss");
-                        $.ajax({
-                            url: SITEURL + "/calendar-events",
-                            data: {
-                                title: event_name,
-                                start: event_start,
-                                end: event_end,
-                                type: 'create'
-                            },
-                            type: "POST",
-                            success: function (data) {
-                                displayMessage("Event created.");
-                                calendar.fullCalendar('renderEvent', {
-                                    id: data.id,
-                                    title: event_name,
+
+            // Fetch events from the server using AJAX GET request
+            $.ajax({
+                url: SITEURL + "/calendar-events",
+                type: "GET",
+                success: function (data) {
+                    // Initialize the FullCalendar with the fetched events
+                    var calendar = $('#full_calendar_events').fullCalendar({
+                        editable: true,
+                        events: data, // Use the fetched events here
+                        displayEventTime: true,
+                        eventRender: function (event, element, view) {
+                            if (event.allDay === 'true') {
+                                event.allDay = true;
+                            } else {
+                                event.allDay = false;
+                            }
+                        },
+                        selectable: true,
+                        selectHelper: true,
+                        select: function (event_start, event_end, allDay) {
+                            var event_name = prompt('Event Name:');
+                            if (event_name) {
+                                var event_start = $.fullCalendar.formatDate(event_start, "Y-MM-DD HH:mm:ss");
+                                var event_end = $.fullCalendar.formatDate(event_end, "Y-MM-DD HH:mm:ss");
+                                $.ajax({
+                                    url: SITEURL + "/calendar-events",
+                                    data: {
+                                        event_name: event_name,
+                                        event_start: event_start,
+                                        event_end: event_end,
+                                        type: 'create'
+                                    },
+                                    type: "POST",
+                                    success: function (data) {
+                                        displayMessage("Event created.");
+                                        calendar.fullCalendar('renderEvent', {
+                                            id: data.id,
+                                            title: event_name,
+                                            start: event_start,
+                                            end: event_end,
+                                            allDay: allDay
+                                        }, true);
+                                        calendar.fullCalendar('unselect');
+                                    }
+                                });
+                            }
+                        },
+                        eventDrop: function (event, delta) {
+                            var event_start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
+                            var event_end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
+                            $.ajax({
+                                url: SITEURL + '/calendar-events/' + event.id,
+                                data: {
+                                    title: event.title,
                                     start: event_start,
                                     end: event_end,
-                                    allDay: allDay
-                                }, true);
-                                calendar.fullCalendar('unselect');
-                            }
-                        });
-                    }
-                },
-                eventDrop: function (event, delta) {
-                    var event_start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
-                    var event_end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
-                    $.ajax({
-                        url: SITEURL + '/calendar-events/' + event.id,
-                        data: {
-                            title: event.title,
-                            start: event_start,
-                            end: event_end,
-                            _method: 'PUT'
+                                    _method: 'PUT'
+                                },
+                                type: "POST",
+                                success: function (response) {
+                                    displayMessage("Event updated");
+                                }
+                            });
                         },
-                        type: "POST",
-                        success: function (response) {
-                            displayMessage("Event updated");
+                        eventClick: function (event) {
+                            var eventDelete = confirm("Are you sure?");
+                            if (eventDelete) {
+                                $.ajax({
+                                    type: "POST",
+                                    url: SITEURL + '/calendar-events/' + event.id,
+                                    data: {
+                                        _method: 'DELETE'
+                                    },
+                                    success: function (response) {
+                                        calendar.fullCalendar('removeEvents', event.id);
+                                        displayMessage("Event removed");
+                                    }
+                                });
+                            }
                         }
                     });
                 },
-                eventClick: function (event) {
-                    var eventDelete = confirm("Are you sure?");
-                    if (eventDelete) {
-                        $.ajax({
-                            type: "POST",
-                            url: SITEURL + '/calendar-events/' + event.id,
-                            data: {
-                                _method: 'DELETE'
-                            },
-                            success: function (response) {
-                                calendar.fullCalendar('removeEvents', event.id);
-                                displayMessage("Event removed");
-                            }
-                        });
-                    }
+                error: function (error) {
+                    console.log("Error fetching events: " + JSON.stringify(error));
                 }
             });
 
